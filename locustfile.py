6f0a63c2a1bab82f7c17baf4d188f3c2b145faa8
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2022-06-19 19:24:01 -0300

    Modified locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,34 +1,41 @@
+from asyncio import tasks
 import time
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, TaskSet
 
-class WebSiteUser(HttpUser):
-# Cuanto Tiempo tarda en visitar un endpoint despues de haber visitado el anterior
-    wait_time = between(1,5)
+class UserBehaviour(TaskSet):
 
-    @task
+    @task(3)
     def cart_get(self):
         self.client.get(url="/cart")
 
-    @task
+    @task(2)
     def cart_put(self):
-        self.client.post(url="/cart",json={
+        self.client.put(url="/cart",json={
         "item":{"id":1, "price":5, "name": "sandwich", "restaurantId": 1},
         "qty": 1})
 
-    @task
+    @task(1)
     def cart_delete(self):
         self.client.delete(url="/cart",json={
         "item":{"id":1, "price":5, "name": "sandwich", "restaurantId": 1},
         "qty": 1})
 
-    @task
+    @task(3)
     def restaurant_get(self):
         self.client.get(url="/restaurants")
 
-    @task
+    @task(3)
     def restaurantNear_get(self):
         self.client.get(url="/restaurantsNear")
 
-    @task
+    @task(3)
     def makeOrder(self):
         self.client.post(url="/makeOrder", json={"restaurantName": "Resto 1"})
+
+
+
+class WebsiteUser(HttpUser):
+    host = "http://localhost:5000"
+    tasks = [UserBehaviour]
+    # The wait_time function is used to determine for how long a simulated user will wait between executing tasks.
+    wait_time = between(1,5)
